8ef4c567e16c11d45472191a5f5efdaa2a50c9ef
Afaroz Alam <afaroz.alam@clarivate.com>
2020-05-23 20:36:57 +0530

    @input decorator

---
+++ src/app/child-component/child-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child-component',
+  templateUrl: './child-component.component.html',
+  styleUrls: ['./child-component.component.scss']
+})
+export class ChildComponentComponent implements OnInit {
+
+  @Input() inputFlag: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
